6f327b818937a8b15d64489db4798551af4b30d4
Troywwww <wjltroy@gmail.com>
2019-01-08 17:46:10 +0800

    fix typo

--- batch_solver.py
+++ batch_solver.py
@@ -3,14 +3,13 @@
 @Date: 2019-01-08 16:43:42
 @LastEditors: Jilong Wang
 @Email: jilong.wang@watrix.ai
-@LastEditTime: 2019-01-08 17:21:17
+@LastEditTime: 2019-01-08 17:46:05
 @Description: file content
 '''
 import os
 import shutil
 
-
-DATA_ROOT = '/home/wangjilong/data/casia-b'
+DATA_ROOT = '/home/wangjilong/data/casia_b'
 video_names = os.listdir(os.path.join(DATA_ROOT, 'videos'))
 video_names.sort()
 names = []
@@ -25,3 +24,4 @@ for i in range(len(names)):
     print("Processing set {}".format(i))
     for v_name in names[i]:
         shutil.copy(os.path.join(DATA_ROOT, 'videos', v_name), os.path.join(DATA_ROOT, 'slice'+str(i), v_name))
+
